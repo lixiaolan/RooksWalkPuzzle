28a0356284704171358ea83c537738b7d41920fe
lee <jeffers.l@gmail.com>
2013-08-27 17:26:42 -0500

    Added android_ref.txt

--- android/bin/OpenGLES20Complete-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/Users/qile/Documents/programming/projects/Rook/android/bin/OpenGLES20Complete-debug-unaligned.apk : \
-/Users/qile/Documents/programming/projects/Rook/android/bin/OpenGLES20Complete.ap_ \
-/Users/qile/Documents/programming/projects/Rook/android/bin/classes.dex \
---
+++ android/bin/RooksWalkPuzzle-debug-unaligned.apk.d
@@ -0,0 +1,3 @@
+/Users/qile/Documents/programming/projects/Rook/android/bin/RooksWalkPuzzle-debug-unaligned.apk : \
+/Users/qile/Documents/programming/projects/Rook/android/bin/RooksWalkPuzzle.ap_ \
+/Users/qile/Documents/programming/projects/Rook/android/bin/classes.dex \
