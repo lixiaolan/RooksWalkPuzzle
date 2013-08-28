558bdbe7c901e8fbdcd0029574e52eddc832380f
lee <jeffers.l@gmail.com>
2013-08-28 14:08:26 -0500

    Working version with cpp native code

--- android/bin/RooksWalkPuzzle.ap_.d
+++
@@ -1,6 +0,0 @@
-/Users/qile/Documents/programming/projects/Rook/android/bin/RooksWalkPuzzle.ap_ \
- : /Users/qile/Documents/programming/projects/Rook/android/res/drawable/ic_launcher.png \
-/Users/qile/Documents/programming/projects/Rook/android/res/layout/main.xml \
-/Users/qile/Documents/programming/projects/Rook/android/res/values/strings.xml \
-/Users/qile/Documents/programming/projects/Rook/android/bin/res/drawable/ic_launcher.png \
-/Users/qile/Documents/programming/projects/Rook/android/bin/AndroidManifest.xml \
--- android/gen/R.java.d
+++ android/gen/R.java.d
@@ -1,6 +1,6 @@
-/home/jain/Repositories/RooksWalkPuzzle/android/gen/com/example/android/opengl/R.java \
- : /home/jain/Repositories/RooksWalkPuzzle/android/res/drawable/ic_launcher.png \
-/home/jain/Repositories/RooksWalkPuzzle/android/res/values/strings.xml \
-/home/jain/Repositories/RooksWalkPuzzle/android/res/layout/main.xml \
-/home/jain/Repositories/RooksWalkPuzzle/android/bin/res/drawable/ic_launcher.png \
-/home/jain/Repositories/RooksWalkPuzzle/android/bin/AndroidManifest.xml \
+/Users/qile/Documents/programming/projects/RooksWalkPuzzle/android/gen/com/example/android/opengl/R.java \
+ : /Users/qile/Documents/programming/projects/RooksWalkPuzzle/android/res/drawable/ic_launcher.png \
+/Users/qile/Documents/programming/projects/RooksWalkPuzzle/android/res/layout/main.xml \
+/Users/qile/Documents/programming/projects/RooksWalkPuzzle/android/res/values/strings.xml \
+/Users/qile/Documents/programming/projects/RooksWalkPuzzle/android/bin/res/drawable/ic_launcher.png \
+/Users/qile/Documents/programming/projects/RooksWalkPuzzle/android/bin/AndroidManifest.xml \
