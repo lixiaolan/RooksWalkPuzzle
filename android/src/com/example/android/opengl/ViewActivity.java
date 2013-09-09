37bcffaa9d9f3d414e3d59f5d89c4c7167a5d220
jain <lalitkumarj@gmail.com>
2013-09-08 20:59:38 -0500

    Massive UI changes.

--- android/src/com/example/android/opengl/ViewActivity.java
+++ android/src/com/example/android/opengl/ViewActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.opengl;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,8 +15,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ViewAnimator;
 import android.widget.ViewSwitcher;
 
 public class ViewActivity extends Activity {
@@ -21,14 +27,16 @@ public class ViewActivity extends Activity {
 	private GLSurfaceView mGLView;
 	private Model mModel;
 	private MyGLRenderer mRenderer;
-	private SharedPreferences gamePrefs;
 	public static final String GAME_PREFS = "ScoreFile";
+	
+	public enum State {MAIN_MENU, GAME, STATS };
+
+	private State state = State.MAIN_MENU;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.viewactivity);
-		gamePrefs = getSharedPreferences(GAME_PREFS, 0);
 		// Create a GLSurfaceView instance and set it
 		// as the ContentView for this Activity
 		if(savedInstanceState != null){
@@ -40,6 +48,13 @@ public class ViewActivity extends Activity {
 		mGLView = (GameView)findViewById(R.id.surface_view);
 		((GameView)mGLView).setMyRenderer(mRenderer);
 
+		String fontPath = "Archistico_Simple.ttf";
+        Button main_menu_dance_button = (Button) findViewById(R.id.main_menu_dance_button);
+        // Loading Font Face
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+        // Applying font
+        main_menu_dance_button.setTypeface(tf);
+		
 	}
 
 	@Override
@@ -66,94 +81,32 @@ public class ViewActivity extends Activity {
 		// If you de-allocated graphic objects for onPause()
 		// this is a good place to re-allocate them.
 		//mGLView.onResume();
-	}
-
-	public boolean onCreateOptionsMenu(Menu menu) {
-		
-		//Somehow the menu items and the default layout all get merged?
-		final ActionBar actionBar = getActionBar();
-		getMenuInflater().inflate(R.menu.viewactivity_actions, menu);		
-		final ViewGroup actionBarLayout = (ViewGroup) getLayoutInflater().inflate(
-				R.layout.viewactivity_actionbar,
-				null);
-
-		// Set up your ActionBar
-		actionBar.setDisplayShowHomeEnabled(false);
-		actionBar.setDisplayShowTitleEnabled(false);
-		actionBar.setDisplayShowCustomEnabled(true);
-		actionBar.setCustomView(actionBarLayout);
-		Chronometer ch = (Chronometer)findViewById(R.id.puzzle_timer);
-		ch.start();
-	
-		
-		return true;
-	} 
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.new_game_action:
-			System.out.println("CLICKED");
-			mModel.resetBoard();    	  
-			break;
-
-		default:
-			break;
-		}
-
-		return true;
-	} 
-
-	
-	public void checkSolution(View view){
-		//THIS IS THE WORST METHOD EVER WRITTEN
-		//Fixes include moving share helper into a fragment?
-		//Use a fragment for the dialog as well I think
-		if(true) {
-			AlertDialog.Builder builder;
-	        builder = new AlertDialog.Builder(this, AlertDialog.THEME_HOLO_DARK);
-	        builder.setTitle("Finished!");
-	        final View v = LayoutInflater.from(this).inflate(R.layout.solved_fragment, null);
-	        builder.setView(v);
-	        final AlertDialog mDialog = builder.create();
-	        mDialog.show();
-	        
-	        Button share = (Button)v.findViewById(R.id.button1);
-	        share.setOnClickListener(new OnClickListener()
-	        {
-	            @Override
-	            public void onClick(View v)
-	            {
-	            	mDialog.dismiss();
-	            	share();
-	            }
-	        });
-	        
-		}
-	}
-
-	public void share() {
-		ShareHelper sh = new ShareHelper(this,"1","2","3","4","http://www.google.com");
-		sh.share();
+		AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this,R.anim.shrink_dance_button_anim);
+	  	set.setTarget((Button)findViewById(R.id.main_menu_dance_button));
+	  	set.end();
 	}
 	
-	/*private void setTime(){
-		SharedPreferences.Editor scoreEdit = gamePrefs.edit();
-		float[] scores = gamePrefs.getFloat("times", "");
-		if(scores.length()>0){
-		    //we have existing scores
-		}
-		else{
-			
-		    //no existing scores
-			
-		}
-	}*/
-	
-	  public void switchView(View v) {
-	    	System.out.println("Click me please");
-	    	ViewSwitcher main = (ViewSwitcher)findViewById(R.id.ViewSwitcher1);
-	    	main.showNext();
+	  public void manageView(View v) {
+		  	if(state==State.MAIN_MENU){
+		  		AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this,R.anim.shrink_dance_button_anim);
+		  		set.setTarget((Button)findViewById(R.id.main_menu_dance_button));
+		  		set.start();
+		  	
+		  		Chronometer ch  = (Chronometer)findViewById(R.id.game_timer);
+		  		ch.setVisibility(View.VISIBLE);
+		  			  	
+		  		Button footsteps  = (Button)findViewById(R.id.footsteps);
+		  		footsteps.setVisibility(View.VISIBLE);
+		  	
+		  		Button stats  = (Button)findViewById(R.id.stats);
+		  		stats.setVisibility(View.INVISIBLE);
+		  		
+		  		state=State.GAME;
+		  	}
+		  	
+		  	
+		  	//ViewAnimator main = (ViewAnimator)findViewById(R.id.ViewAnimator1);
+	    	//main.showNext();
 	    }
 }
 
