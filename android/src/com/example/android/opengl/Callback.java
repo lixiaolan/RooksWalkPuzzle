07579f7b9735691f009d62b51bbb2dca80a1a3ea
jain <lalitkumarj@gmail.com>
2013-09-18 23:18:58 -0500

    GameMEnu added.

---
+++ android/src/com/example/android/opengl/Callback.java
@@ -0,0 +1,5 @@
+package com.example.android.opengl;
+
+public abstract class Callback {
+	public abstract void callback(int val);	
+}
---
+++ android/src/com/example/android/opengl/GameMenu.java
@@ -0,0 +1,73 @@
+package com.example.android.opengl;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import android.os.SystemClock;
+
+class GameMenu{
+
+    public MenuTile[] menuTiles;
+    public MenuTile centerTile;
+    public float radius;
+    public float tiltAngle;
+    public float[] tilesCenter = new float[3];
+    public float tilesSize;
+    public long refTime;
+    
+    public GameMenu(float[] pos, float size, String[] textures, String centerTexture) {
+	tilesSize = size;
+	tilesCenter = pos;
+	tiltAngle = (float)(-1*Math.PI)/4.0f;
+	radius = size*4.0f;
+	menuTiles = new MenuTile[textures.length];
+	for (int i = 0; i < menuTiles.length; i++) {
+	    float tmpsin = (float)Math.sin(i*Math.PI/5.0f);
+	    float tmpcos = (float)Math.cos(i*Math.PI/5.0f);
+	    float Sx = tilesCenter[0] + radius*tmpcos;
+	    float Sy = tilesCenter[1] + radius*tmpsin;
+	    float center[] = { Sx, Sy, 0.0f};
+	    menuTiles[i] = new MenuTile(center, tilesSize, textures[i]);
+	}
+	centerTile = new MenuTile(tilesCenter, tilesSize, centerTexture);	
+	refTime = System.currentTimeMillis();    
+    }
+	
+    public int touched(float[] pt) {
+    	
+    	for (int i = 0; i < menuTiles.length; i++) {
+    		if (menuTiles[i].touched(pt)) {	    	
+    			return i+1;
+    		}
+    	}
+    	if (centerTile.touched(pt)) {
+    		return 0; 
+    	}
+    	return -1;
+    }
+
+    public void draw(MyGLRenderer r) {
+	
+	    animate();
+	    for (int i = 0; i < menuTiles.length; i++) {
+		menuTiles[i].draw(r);
+	    }
+	    centerTile.draw(r);
+    }
+
+
+    public void animate() {
+	long time = System.currentTimeMillis() - refTime;  	
+	float tRadius = Math.min( radius, (radius / 250.0f) * ((int) time) ); 
+	    for (int i = 0; i < menuTiles.length; i++) {     
+		float tmpsin = (float)Math.sin(tiltAngle+i*Math.PI/5.0f);
+		float tmpcos = (float)Math.cos(tiltAngle+i*Math.PI/5.0f);
+		float Sx = tilesCenter[0] + tRadius*tmpcos;
+		float Sy = tilesCenter[1] + tRadius*tmpsin;
+		float center[] = { Sx, Sy, 0.0f};
+		menuTiles[i].center = center;
+	
+	}
+    }
+}
