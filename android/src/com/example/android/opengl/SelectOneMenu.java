4c7b47137262ce20a83c57b099c3e75102e90692
jain <lalitkumarj@gmail.com>
2013-09-29 11:55:11 -0500

    Have basic touching to transition between screens implemented.

---
+++ android/src/com/example/android/opengl/SelectOneMenu.java
@@ -0,0 +1,41 @@
+package com.example.android.opengl;
+
+
+class SelectOneMenu extends GameMenu{
+
+   
+    
+    public SelectOneMenu(float[] pos, float size, String[] textures) {
+    	initialize(pos, size, textures);
+    }
+	
+    
+    
+    private void initialize(float[] pos, float size, String[] textures) {
+    	tilesSize = size;
+    	tilesCenter = pos;
+    	radius = 0.0f;
+    	
+    	menuTiles = new MenuTile[textures.length];
+    	for (int i = 0; i < menuTiles.length; i++) {
+    	    float Sx = tilesCenter[0];
+    	    float Sy = tilesCenter[1];
+    	    float center[] = { Sx, Sy, 0.0f};
+    	    menuTiles[i] = new MenuTile(center, tilesSize, textures[i]);
+    	}
+    	centerTile = new MenuTile(tilesCenter, tilesSize, TextureManager.CLEAR);	
+    	centerTile.setTextures(TextureManager.CLEAR, TextureManager.CLEAR);
+    }
+    
+    public void animate() {
+	    for (int i = 0; i < menuTiles.length; i++) {     
+		float Sx = tilesCenter[0];
+		float Sy = tilesCenter[1];
+		float center[] = { Sx, Sy, 0.0f};
+		menuTiles[i].center = center;	
+	}
+    }
+    
+   
+
+}
