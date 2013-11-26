ad9b60d48bfc17ec0effc5b72215f85eb819ba83
jain <lalitkumarj@gmail.com>
2013-11-26 14:13:21 -0600

    Added everything but php binaries.

--- websitejinja/main.py
+++ websitejinja/main.py
@@ -15,12 +15,25 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 DEFAULT_DB_NAME = "EMAILS_DB"
+FEEDBACK_DB_NAME = "FEEDBACK_DB"
 
 
 print os.path.dirname(TEMPLATE)
 def email_key(db_name=DEFAULT_DB_NAME):
     """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
     return ndb.Key('Emails', db_name)
+
+def feedback_key(db_name=FEEDBACK_DB_NAME):
+    """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
+    return ndb.Key('Feedback', db_name)
+
+class Feedback(ndb.Model):
+    """Models an individual Guestbook entry with author, content, and date."""
+    email = ndb.StringProperty()
+    phone_model = ndb.StringProperty()
+    feedback = ndb.TextProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
 
 
 class Email(ndb.Model):
@@ -47,6 +60,21 @@ class EmailHandler(webapp2.RequestHandler):
     #query_params = {'emailbook_name': emailbook_name}
     #self.redirect('/')
 
-application = webapp.WSGIApplication ([('/(.*html)?', MainHandler),('/email',EmailHandler)], debug=True)
+class FeedbackHandler(webapp2.RequestHandler):
+  def post(self):
+    email = self.request.get('email_input', FEEDBACK_DB_NAME)
+    email_input = self.request.get('email_input')
+    phone_model = self.request.get('phone_model_input')
+    feedback = self.request.get('feedback_input')
+    #print "GOTHIS: "+email_input+" "+phone_model+" "+feedback
+    
+    feedbackEntity = Feedback(parent=feedback_key(email+feedback[0:min(10,len(feedback))]))
+    feedbackEntity.email = self.request.get('email_input')
+    feedbackEntity.phone_model = self.request.get('phone_model_input')
+    feedbackEntity.feedback = self.request.get('feedback_input')
+    
+    feedbackEntity.put()
+
+application = webapp.WSGIApplication ([('/(.*html)?', MainHandler),('/email',EmailHandler),('/feedback',FeedbackHandler)], debug=True)
 #  util.run_wsgi_app (application)
 
