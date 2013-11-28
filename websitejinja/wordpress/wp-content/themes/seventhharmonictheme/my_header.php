a08224ef99931e7f9319b089ca591db4dabb263b
jain <lalitkumarj@gmail.com>
2013-11-27 18:21:19 -0600

    Working blog.

---
+++ websitejinja/wordpress/wp-content/themes/seventhharmonictheme/my_header.php
@@ -0,0 +1,35 @@
+    <header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+      <div class="container">
+	<div class="navbar-header">
+	  <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+	  </button>
+	  <img src="../img/7ha.png" class="imgClip"/>
+	</div>
+	<nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+	  <ul class="nav navbar-nav">
+            <li class>
+              <a href="../index.html">Seventh Harmonic</a>
+            </li class>
+	    <li class>
+	      <a href="../BeeLine.html"> Bee-Line </a>
+	    </li>
+	    <li class>
+	      <a href="../Beta.html"> Beta/Bug Report </a>
+	    </li>
+	    <li class>
+	      <a href="../blog/">Blog </a>
+	    </li>
+
+	    <li class>
+	      <a href="../index.html"> About Us </a>
+	    </li>
+	    <li class>
+	      <a href="../index.html"> Contact Us </a>
+	    </li>
+	</nav>
+      </div>
+    </header>
